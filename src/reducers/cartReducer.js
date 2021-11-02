18993424fdd9080d0232c38b20bf25e29daaa5b7
immanuel3333 <immanuel.siburian80@gmail.com>
2021-11-02 15:09:52 +0700

    submit

---
+++ src/reducers/cartReducer.js
@@ -0,0 +1,163 @@
+import Item1 from "../images/avocado.jpg";
+import Item2 from "../images/carrot.jpg";
+import Item3 from "../images/corn.jpg";
+import Item4 from "../images/garlic.jpg";
+import Item5 from "../images/red-chili.jpg";
+import Item6 from "../images/tomato.jpg";
+import {
+  ADD_TO_CART,
+  REMOVE_ITEM,
+  SUB_QUANTITY,
+  ADD_QUANTITY,
+  ADD_SHIPPING,
+  SUB_SHIPPING,
+} from "../actions/action-types/cartActions";
+import produce from "immer";
+
+const initState = {
+  items: [
+    {
+      id: 1,
+      title: "Avocado",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",
+      price: 110,
+      img: Item1,
+      quantity: 0,
+    },
+    {
+      id: 2,
+      title: "Carrot",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",
+      price: 80,
+      img: Item2,
+      quantity: 0,
+    },
+    {
+      id: 3,
+      title: "Corn",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",
+      price: 120,
+      img: Item3,
+      quantity: 0,
+    },
+    {
+      id: 4,
+      title: "Garlic",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",
+      price: 260,
+      img: Item4,
+      quantity: 0,
+    },
+    {
+      id: 5,
+      title: "Red Chili",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",
+      price: 160,
+      img: Item5,
+      quantity: 0,
+    },
+    {
+      id: 6,
+      title: "Tomato",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",
+      price: 90,
+      img: Item6,
+      quantity: 0,
+    },
+  ],
+  addedItems: [],
+  total: 0,
+};
+
+const cartReducer = (state = initState, action) => {
+  //INSIDE HOME COMPONENT
+  if (action.type === ADD_TO_CART) {
+    const nextState = produce(state, (draf) => {
+      const id = action.payload;
+      if (draf.addedItems.find((item) => item.id === id)) {
+        const found = draf.addedItems.find((item) => item.id === id);
+        found.quantity++;
+        draf.total += parseInt(found.price);
+      } else {
+        const found = draf.items.find((item) => item.id === id);
+        found.quantity++;
+        draf.addedItems.push(found);
+        draf.total += parseInt(found.price);
+      }
+    });
+
+    return nextState;
+  }
+
+  if (action.type === REMOVE_ITEM) {
+    // Add code here
+    const nextState = produce(state, (draf) => {
+      const id = action.payload;
+      let itemToRemove = draf.addedItems.find((item) => item.id === id);
+      if (itemToRemove) {
+        let new_items = draf.addedItems.filter((item) => item.id !== id);
+        let newTotal = draf.total - itemToRemove.price * itemToRemove.quantity;
+        return {
+          ...state,
+          addedItems: new_items,
+          total: newTotal,
+        };
+      }
+    });
+    return nextState;
+  }
+
+  //INSIDE CART COMPONENT
+  if (action.type === ADD_QUANTITY) {
+    // Add code here
+    const nextState = produce(state, (draf) => {
+      const id = action.payload;
+      const found = draf.addedItems.find((item) => item.id === id);
+      found.quantity++;
+      draf.total += parseInt(found.price);
+    });
+
+    return nextState;
+  }
+  if (action.type === SUB_QUANTITY) {
+    // Add code here
+    const nextState = produce(state, (draf) => {
+      const id = action.payload;
+      const findItem = draf.addedItems.find((item) => item.id === id);
+      if (findItem.quantity === 1) {
+        let newItem = draf.addedItems.filter((item) => item.id !== id);
+        return {
+          ...state,
+          addedItems: newItem,
+        };
+      } else {
+        findItem.quantity--;
+        draf.total = draf.total - findItem.price;
+      }
+    });
+
+    return nextState;
+  }
+
+  if (action.type === ADD_SHIPPING) {
+    // Add code here (OPTIONAL)
+    const nextState = produce(state, (draf) => {
+      draf.total += 6;
+    });
+
+    return nextState;
+  }
+
+  if (action.type === SUB_SHIPPING) {
+    // Add code here (OPTIONAL)
+    const nextState = produce(state, (draf) => {
+      draf.total -= 6;
+    });
+
+    return nextState;
+  } else {
+    return state;
+  }
+};
+
+export default cartReducer;
